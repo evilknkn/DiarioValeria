f9417066d8d18c9dc6ea078f3966d347014a0309
Omar Caballero <omar.caballero@aventuracp.com.mx>
2015-04-21 18:11:28 -0500

    primer commit

---
+++ application/views/app.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Mi diario erótico</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="<?php echo base_url()?>assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="<?php echo base_url()?>assets/css/business-casual.css" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Great+Vibes">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link href="<?php echo base_url()?>assets/css/styles.css" rel="stylesheet">
+  <style>
+      .titulo {
+        font-family: 'Great Vibes', serif;
+        font-size: 60px;
+        color:#fff;
+      }
+      .links
+      {
+        text-decoration: none;
+        color: #fff;
+      }
+      /* unvisited link */
+        
+    </style>
+</head>
+
+<body>
+
+    <div class="text-center titulo"><a href="<?php echo base_url()?>" class="title"> Mi Diario Erótico </a></div>
+    <div class="address-bar"> <a href="tel:+52-556-383-5101" style="color:#fff;text-decoration:none">Contrataciones 5543731944</a> </div>
+
+    <!-- Navigation -->
+    <?php echo $this->load->view('menu')?>
+
+    <div class="container">
+        <?php echo $this->load->view($body)?>
+    </div>
+    <!-- /.container -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; Mi diario Erótico 2015</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="<?php echo base_url()?>assets/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo base_url()?>assets/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
